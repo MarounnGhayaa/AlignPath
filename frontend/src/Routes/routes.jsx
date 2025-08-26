a61618a64d649accee008fd594b1ea518eccc9e6
MarounnGhayaa <marounghaya@icloud.com>
2025-08-26 20:27:00 +0300

    Protected my routes on the frontend

--- frontend/src/Routes/routes.jsx
+++ frontend/src/Routes/routes.jsx
@@ -1,8 +1,11 @@
 import "../Styles/App.css";
 import { Routes, Route } from "react-router-dom";
+
+import MainLayout from "../Layouts/MainLayout";
+import ProtectedRoute from "../Layouts/ProtectedRoutes";
+
 import Landing from "../Pages/Landing";
 import Auth from "../Pages/Auth";
-import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home";
 import Explore from "../Pages/Explore";
 import Network from "../Pages/Network";
@@ -17,13 +20,62 @@ const MyRoutes = () => {
       <Route path="/" element={<Landing />} />
       <Route path="/auth" element={<Auth />} />
       <Route element={<MainLayout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/network" element={<Network />} />
-        <Route path="/path" element={<Path />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/pathNested" element={<PathNested />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/explore"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <Explore />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/network"
+          element={
+            <ProtectedRoute allowedRoles={["mentor"]}>
+              <Network />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/path"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <Path />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/pathNested"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <PathNested />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute allowedRoles={["student", "mentor"]}>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   );
